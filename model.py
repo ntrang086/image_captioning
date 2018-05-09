b79627bc44d6c302f66abb0d494bef0e8f5feca2
Trang Nguyen <ntrang086@gmail.com>
2018-05-09 17:29:54 -0400

    feat: Create a CNN encoder and RNN decoder

---
+++ model.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        """Load the pretrained ResNet-50 and replace top fc layer."""
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        modules = list(resnet.children())[:-1] 
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions):
+        captions = captions[:,:-1]
+        embeddings = self.embed(captions)
+        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(inputs)
+        outputs = self.linear(hiddens)
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        """Accept pre-processed image tensor (inputs) and return predicted 
+        sentence (list of tensor ids of length max_len).
+        """
+        sampled_ids = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            # Get the index (in the vocabulary) of the most likely integer that
+            # represents a word
+            predicted = outputs.argmax(1)
+            sampled_ids.append(predicted.item())
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        return sampled_ids
