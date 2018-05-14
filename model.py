b575f8f6b95745cdd9a7baa3c4b89172fb6b2ea2
Trang Nguyen <ntrang086@gmail.com>
2018-05-14 13:33:40 -0400

    feat: Add batch normalization to EncoderCNN

--- model.py
+++ model.py
@@ -7,26 +7,31 @@ class EncoderCNN(nn.Module):
         """Load the pretrained ResNet-50 and replace top fc layer."""
         super(EncoderCNN, self).__init__()
         resnet = models.resnet50(pretrained=True)
-        for param in resnet.parameters():
-            param.requires_grad_(False)
         modules = list(resnet.children())[:-1] 
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
 
     def forward(self, images):
-        features = self.resnet(images)
+        """Extract feature vectors from input images."""
+        with torch.no_grad():
+            features = self.resnet(images)
         features = features.view(features.size(0), -1)
         features = self.embed(features)
+        features = self.bn(features)
         return features
+
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        """Set the hyper-parameters and build the layers."""
         super(DecoderRNN, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, features, captions):
+        """Decode image feature vectors and generates captions."""
         captions = captions[:,:-1]
         embeddings = self.embed(captions)
         inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
