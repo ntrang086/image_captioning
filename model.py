d86c45ba02e0b9c6c06dd85e7febeeab421320c1
Trang Nguyen <ntrang086@gmail.com>
2018-06-06 15:13:46 -0400

    feat: Sample captions using beam search

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torch.nn import functional as F
+
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
@@ -40,8 +42,9 @@ class DecoderRNN(nn.Module):
         return outputs
 
     def sample(self, inputs, states=None, max_len=20):
-        """Accept pre-processed image tensor (inputs) and return predicted 
-        sentence (list of tensor ids of length max_len).
+        """Accept a pre-processed image tensor (inputs) and return predicted 
+        sentence (list of tensor ids of length max_len). This is the greedy
+        search approach.
         """
         sampled_ids = []
         for i in range(max_len):
@@ -54,3 +57,37 @@ class DecoderRNN(nn.Module):
             inputs = self.embed(predicted)
             inputs = inputs.unsqueeze(1)
         return sampled_ids
+
+    def sample_beam_search(self, inputs, states=None, max_len=20, beam_width=5):
+        """Accept a pre-processed image tensor and return the top predicted 
+        sentences. This is the beam search approach.
+        """
+        # Top word idx sequences and their corresponding inputs and states
+        idx_sequences = [[[], 0.0, inputs, states]]
+        for _ in range(max_len):
+            # Store all the potential candidates at each step
+            all_candidates = []
+            # Predict the next word idx for each of the top sequences
+            for idx_seq in idx_sequences:
+                hiddens, states = self.lstm(idx_seq[2], idx_seq[3])
+                outputs = self.linear(hiddens.squeeze(1))
+                # Transform outputs to log probabilities to avoid floating-point 
+                # underflow caused by multiplying very small probabilities
+                log_probs = F.log_softmax(outputs, -1)
+                top_log_probs, top_idx = log_probs.topk(beam_width, 1)
+                top_idx = top_idx.squeeze(0)
+                # create a new set of top sentences for next round
+                for i in range(beam_width):
+                    next_idx_seq, log_prob = idx_seq[0][:], idx_seq[1]
+                    next_idx_seq.append(top_idx[i].item())
+                    log_prob += top_log_probs[0][i].item()
+                    # Indexing 1-dimensional top_idx gives 0-dimensional tensors.
+                    # We have to expand dimensions before embedding them
+                    inputs = self.embed(top_idx[i].unsqueeze(0)).unsqueeze(0)
+                    all_candidates.append([next_idx_seq, log_prob, inputs, states])
+            # Keep only the top sequences according to their total log probability
+            ordered = sorted(all_candidates, key=lambda x: x[1], reverse=True)
+            idx_sequences = ordered[:beam_width]
+        return [idx_seq[0] for idx_seq in idx_sequences]
+
+    
